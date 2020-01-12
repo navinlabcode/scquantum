594ffaca4a2fa550c50d56d734509517da26f1a5
Alex Davis <ajdavis2@mdanderson.org>
2020-01-12 14:29:11 -0600

    Refactored version of prof2invals for segmentation

--- R/ploidy_estimation.R
+++ R/ploidy_estimation.R
@@ -63,68 +63,130 @@ gat <- function(x, iod)
   # When index of dispersion is above 4, transformation will be imaginary for
   # low inputs. So, you need to check if it's real-valued. If it's imaginary,
   # return NA.
-  ifelse(x + (4-iod)/8 > 0,
+  ifelse(x + (4-iod)/8 < 0,
+    NA,
     # Due to vectorization, this will get executed even when the result is
     # imaginary, resulting in a warning. Therefore, suppress warnings.
-    suppressWarnings((2 / sqrt(iod)) * sqrt(x + (4-iod)/8)),
-    NA
+    suppressWarnings((2 / sqrt(iod)) * sqrt(x + (4-iod)/8))
   )
 }
 
 #' @useDynLib scquantum tf_dp_wrapper
-tf.dp <- function(y, lam)
+tf.dp <- Vectorize(function(y, lam)
 {
   n <- length(y)
   stopifnot(length(lam)==1)
   .C("tf_dp_wrapper", n=as.integer(n), y=as.numeric(y), lam=as.double(lam)[1], beta=numeric(n))$beta
-}
+}, "lam")
 
-# Refactoring prof2invals
-
-# Version of prof2invals which records more information
-# Output table has chrom, seg_start_relpos, seg_end_relpos, length, location,
-# ratio, and ratio_sd
-prof2invals <- function(inprof, lambda)
+# Segment a profile, and summarize the segments. Output has columns lambda,
+# mean, se, length, start_index, and end_index. Segmentation is performed for
+# each input penalty value ("lambda"), and each row int he output represents a
+# segment, giving the estimated mean of the segment, the standard error of the
+# mean, and the number of elements which were in the segment.
+segment.summarize <- function(inprof, lambda, trans, seg, loc, se)
 {
+  # If the input penalties don't have names, name them. This is intended to make
+  # grouping by penalty safe without worrying about flaoting point problems.
   # Transform to try and get data which are normally distributed around a
   # segment-specific mean, with the same variance for each segment
-  transformed.profile <- log2(inprof/mean(inprof) + 0.15)
-  # Split up on chromosome boundaries (it should really be arm boundaries, but
-  # that can wait)
-  chrom.profiles <- split(transformed.profile, chrom.column)
-  # For each penalty value, get a segmented profile in long format
-  segmented.profiles <-
-    Reduce(cbind, lapply(chrom.profiles, function(x)
-                         flsaGetSolution(flsa(x), lambda2=lambda)))
-  # For each penalty value, number bins according to what segment they're in
-  is.newseg <- t(apply(segmented.profiles, 1, function(segmented.profile)
-                       c(TRUE, (abs(diff(segmented.profile)) > 0.1) | (diff(chrom.column) != 0))))
-  segnum <- t(apply(is.newseg, 1, cumsum))
-  # Get a location estimate for each segment, and the length of each segment
-  rownames(segnum) <- sprintf("lambda%.5f", lambda)
-  loc.length.table <- as_tibble(t(segnum)) %>%
-    mutate(bincounts=inprof) %>%
-    bind_cols(bin.start.end) %>%
-    gather(key="penalty", value="segnum", -bincounts,
-           -chrom, -bin.start, -bin.end, -bin.number,
-           -abs_start, -abs_end) %>%
-    group_by(penalty, segnum) %>%
-    summarize(length=length(bincounts), location=median(bincounts),
-              start_chrom = min(chrom), end_chrom = max(chrom),
-              start_pos = min(bin.start), end_pos = max(bin.end),
-              abs_start = min(abs_start), abs_end = max(abs_end)) %>%
-    ungroup()
-  # Calculate index of dispersion of the profile
-  iod <- diffsum.tm.2(inprof)
-  # Calculate mean of the part of the profile that's included
-  region.total <- with(loc.length.table,
-                       sum(length * location) / sum(length))
-  # Ratios and standard deviation of the ratios
-  loc.length.weight.ratio.table <-
-    dplyr::mutate(loc.length.table,
-      ratio=location / region.total,
-      ratio_sd = sqrt(pi/2) * sqrt(iod * location / length) / region.total)
-  return(loc.length.weight.ratio.table)
+  transformed.profile <- trans(inprof)
+
+  # For each penalty value, get a segmented profile in long format--that is, of
+  # the same length of the original profile, but piecewise constant. Each
+  # segmented profile, corresponding to a penalty value, will be a column of a
+  # matrix.
+  nlambda <- length(lambda)
+  if (nlambda > 1)
+  {
+    segmented.profiles <- seg(transformed.profile, lambda)
+  } else if (nlambda == 1)
+  # If lambda is of length 1, by default we will get an array, not a matrix, and
+  # it needs to be converted to a matrix for downstream stuff to work
+  {
+    segmented.profiles <- as.matrix(seg(transformed.profile, lambda), ncol=1)
+  } else
+  # What's the remaining case? Length is 0. Conceptually I could return a table
+  # with no rows in that case, but the function is not really supposed to do
+  # nothing and not segment anything, so I'll make that an error case
+  {
+    stop("No penalty values given")
+  }
+
+  # For each penalty value, number elements of the profile according to what
+  # segment they're in
+  segnums <- apply(segmented.profiles, 2, function(segmented.profile)
+    cumsum(c(TRUE, abs(diff(segmented.profile)) > 0.1))
+  )
+
+  # Summarize the segments, recording three numbers: estimate the mean of each
+  # segment, the standard error of the mean, and record the length of the
+  # segment
+  means <- lapply(1:nlambda, function(i)
+    tapply(inprof, segnums[,i], loc)
+  )
+  standard.errors <- lapply(1:nlambda, function(i)
+    tapply(inprof, segnums[,i], se)
+  )
+  lengths <- lapply(1:nlambda, function(i)
+    tapply(inprof, segnums[,i], length)
+  )
+  # From the lengths, get the start and end indices
+  end.indices <- lapply(lengths, cumsum)
+  start.indices <- lapply(end.indices, function(x) c(0, x[-length(x)]) + 1)
+
+  Reduce(rbind, lapply(1:length(lambda), function(i)
+    data.frame(penalty = lambda[i], mean=means[[i]], se=standard.errors[[i]],
+               length=lengths[[i]], start_index = start.indices[[i]], end_index = end.indices[[i]])
+  ))
+}
+
+prof2invals.refactored <- function(
+  # The input required for segmentation: the profile to be segmented, and the
+  # penalty values for the segmentation
+  inprof, lambda,
+  # The input required for annotation: the data frame containing the
+  # annotations, and the names of the columns which are going to be used. This
+  # also affects the segmentation, since chromosome boundaries are always
+  # segment boundaries.
+  annotations, chrom.colname, bin.start.colname, bin.end.colname)
+{
+  # Estimate the index of dispersion, which will be used for estimating standard
+  # errors of segment means, and for transformation
+  iod.est <- scquantum:::timeseries.iod(inprof)
+
+  # Split the annotations into two parts, one which will be accessed using the
+  # segment start indices, and the other which will be accessed using the
+  # segment end indices. The chromosome column can go in either one of these,
+  # but of course the bin start positions need to go in the first, and the bin
+  # end positions need to go in the second.
+  left.annotations <- annotations[,c(chrom.colname, bin.start.colname),drop=FALSE]
+  right.annotations <- annotations[,bin.end.colname,drop=FALSE]
+  # Name them according to what I want the column names in the output to be
+  colnames(left.annotations) <- c("chrom", "seg_start_relpos")
+  colnames(right.annotations) <- "seg_end_relpos"
+
+  # Segment the profile and annotate the results
+  annotated.segmented.counts <- Reduce(rbind, mapply(function(segments, annotations1, annotations2)
+    cbind(annotations1[segments$start_index,,drop=FALSE], annotations2[segments$end_index,,drop=FALSE], segments),
+    # Split the profile by chromosome and segment each chromosome separately,
+    # guaranteeing that chromosome boundaries are segment boundaries
+    tapply(ubers.joined.local$gc_bincounts[[3]], annotations[[chrom.colname]], scquantum:::segment.summarize, lambda=lambda,
+      # The functions to be used to transform the data, to segment it, to estimate
+      # the segment means, and to estimate the standard error of the means
+      trans=function(x) log2(x / mean(x) + 0.15),
+      seg = scquantum:::tf.dp, loc = median,
+      se = function(x) sqrt(pi/2) * sqrt(iod.est * median(x) / length(x))
+    ),
+    # Chromosome annotations and bin start annotations, split by chromosome
+    split(left.annotations, annotations[[chrom.colname]]),
+    # Bin end annotations, split by chromosome
+    split(right.annotations, annotations[[chrom.colname]]),
+    # Can be read either as a statement that the output is to be returned as a
+    # list, or a sarcastic comment about how the code was written
+    SIMPLIFY=FALSE))
+
+  return(annotated.segmented.counts[,c("chrom", "seg_start_relpos", "seg_end_relpos", "penalty", "mean", "se", "length")])
 }
 
 ### Empirical characteristic functions and maxima
